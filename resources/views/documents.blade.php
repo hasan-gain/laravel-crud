348dc41d33403d673eebc8325631e8f03d918325
Shishir <shishir@Asaduzzamans-MacBook-Air.local>
2023-03-14 20:17:40 +0600

    update

---
+++ resources/views/documents.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.client')
+@section('content')
+<section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Comming soon
+      </h1>
+  </div>
+</section>
+@endsection
