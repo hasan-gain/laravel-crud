6539786cf08cb60aa24fdc932086591450e6cdff
Eshan <eshan@eshan4gain.local>
2023-03-14 17:30:49 +0600

    post api updated

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        if ($this->method() === 'POST') {
+            return [
+                'title' => ['required', 'string', 'max:255'],
+                'content' => ['required', 'max:2500'],
+            ];
+        }else {
+            return [
+                'title' => ['required', 'string', 'max:255'],
+                'content' => ['required', 'max:2500'],
+            ];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,5 +23,7 @@ Route::controller(AuthController::class)->group(function(){
 });
 
 Route::middleware('auth:sanctum')->group( function () {
+    Route::get('me', [UserController::class, 'me']);
     Route::apiResource('users', UserController::class);
+    Route::apiResource('posts', PostController::class);
 });
