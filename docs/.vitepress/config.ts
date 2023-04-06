8f629277bebab6ec5b0714d38f20e7cd67b93d5f
Emrozgs <emroz@gain.media>
2023-04-06 10:43:30 +0600

    Modified the theme to match our app's look and feel

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "crud-docs",
-  outDir: '../public/docs',
-  base: process.env.NODE_ENV === 'local' ? '' : '/docs/',
-  description: "Crud documentation",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+    title: 'Core Documentation',
+    outDir: '../public/docs',
+    base: process.env.NODE_ENV === 'local' ? '' : '/docs/',
+    description: 'Crud documentation',
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Home', link: '/'},
+            // { text: 'Examples', link: '/markdown-examples' }
+        ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+        sidebar: [
+            {
+                text: '',
+                items: [
+                    // { text: 'Markdown Examples', link: '/markdown-examples' },
+                    {text: 'Avatar', link: '/api-examples'},
+                    {text: 'Modal', link: '/api-examples'},
+                    {text: 'Input', link: '/api-examples'},
+                    {text: 'FormGroup', link: '/api-examples'},
+                    {text: 'Tabs ', link: '/api-examples'},
+                ],
+            },
+        ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'},
+        ],
+    },
+});
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
----
+<!-- ---
 # https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
@@ -23,3 +23,16 @@ features:
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
 
+---
+outline: deep
+--- -->
+
+# Component Documentation
+
+Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+
+Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+
+``` lua
+print('this is some lua code lol')
+```
